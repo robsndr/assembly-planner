49c6e7c3116de75fa7339d9865f4e7f255e40936
symm3try <robertsander22@yahoo.com>
2019-10-26 01:46:18 +0200

    Update structure

--- assemblyPlanner/Source/main.cpp
+++ assemblyPlanner/Source/main.cpp
@@ -2,48 +2,7 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
-
-
-template <class TypeId, class TypeData> 
-class Connector{
-public:
-    Connector(Node * source, Node * sink);
-    Connector(Node * source, std::vector<);
-    Connector(Node * from, Node * to, TypeData value);
-
-    TypeData getData(void);
-    std::vector<Node*> & getSuccessors(void);
-
-private:
-    TypeData data_;
-};
-
-
-template <class TypeId, class TypeData> 
-class Node{
-public:
-    Node(void);
-    Node(TypeId identifier);
-    Node(TypeId identifier, TypeData data);
-
-    bool hasSuccessor(void);
-    bool hasPredecessor(void);
-    int addPredecessor(Connector * predecessor);
-    int addSuccessor(Connector * successor);
-    Edge * removeSuccessor(TypeId identifier);
-
-
-    std::vector< Edge* > getSuccessors(void);
-    std::vector< Edge* > getPredecessors(void);
-
-    bool visited = false;
-
-private:
-    std::vector<Connector*> parents_;
-    std::vector<Connector*> children_; 
-    TypeId id_;
-    TypeData data_;
-};
+#include "node.hpp"
 
 int main(void){
 
