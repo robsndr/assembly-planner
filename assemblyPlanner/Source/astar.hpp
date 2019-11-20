5ef9ab6fe4eddabd2ad474c93d2f008773bcc8c4
symm3try <robertsander22@yahoo.com>
2019-11-20 20:05:37 +0100

    Begin AStar refactor on new branch.

---
+++ assemblyPlanner/Source/astar.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <climits>
+#include <set>
+#include <vector>
+#include <queue>
+
+
+class AStar{
+private:
+    /* data */
+public:
+    AStar(/* args */);
+    ~AStar();
+};
+
+AStar::AStar(/* args */){
+}
+
+AStar::~AStar(){
+}
