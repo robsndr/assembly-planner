1766e24fbcbf1f63097cd32a2beabae2ff9a4d08
symm3try <robertsander22@yahoo.com>
2020-04-06 14:14:11 +0100

    Further cleanup

--- src/astar.hpp
+++ src/astar.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
 #include <iostream>
-#include <climits>
-#include <set>
-#include <vector>
 #include <queue>
 
 #include "expander.hpp"
 
-/* Comparator function.
-    Used to sort the priosirty queue inside AStarSearch.
-    Orders elements by ascending f_score.
-**/
+// Comparator used to sort the A* priotity queue
 struct LessThan
 {
     bool operator()(const Node<SearchData> *lhs, const Node<SearchData> *rhs) const
@@ -20,14 +14,13 @@ struct LessThan
     }
 };
 
-/* Class representing the A* Search Algorithm.
-    It executes the search on a given graph.
-    The provided Exapander-Object is used to perform the Node-Expansion step.
-**/
+
+// Class used to execute the search on a given graph.
+// The expander is used to perform the node-expansion step of the hypergraph.
 struct AStarSearch
 {
     AStarSearch(Graph<AssemblyData,EdgeData>& assembly);
-    ~AStarSearch() = default;
+
     Node<SearchData>* search(Graph<SearchData,EdgeData>&, Node<SearchData>*, NodeExpander&);
     bool isGoal(Graph<AssemblyData,EdgeData>&, Node<SearchData>*);
     double calc_hscore(Node<SearchData>* current);
@@ -36,10 +29,7 @@ struct AStarSearch
     Graph<AssemblyData,EdgeData>& assembly_;
 };
 
-/* Check if given sueprnode is Goal.
-   Check whether a given supernode inside the A* search 
-   has any subassemblies to continue the search.
-**/
+// Check if given sueprnode is Goal.
 bool AStarSearch::isGoal(Graph<AssemblyData,EdgeData>& graph, Node<SearchData>* current)
 {
     for (auto &x : current->data.subassemblies)
@@ -52,11 +42,6 @@ bool AStarSearch::isGoal(Graph<AssemblyData,EdgeData>& graph, Node<SearchData>* 
     return true;
 }
 
-/* Calculate the h_Score needed for the A* search.
-    Is declared as a member of the NodeData contained within Nodes.
-    Placed within this files due to linking issues.
-    //TODO: Move into containers.hpp and fix linking issues.
-**/
 double AStarSearch::calc_hscore(Node<SearchData>* current)
 {
     std::size_t maximum_length_subassembly = 0;
@@ -71,11 +56,6 @@ double AStarSearch::calc_hscore(Node<SearchData>* current)
     return temp;
 }
 
-/* Calculate the f_Score needed for the A* search.
-    Is declared as a member of the NodeData contained within Nodes.
-    Placed within this files due to linking issues.
-    //TODO: Move into containers.hpp and fix linking issues.
-**/
 double AStarSearch::calc_fscore(Node<SearchData>* current)
 {
     return current->data.g_score + current->data.h_score;
@@ -84,23 +64,22 @@ double AStarSearch::calc_fscore(Node<SearchData>* current)
 
 AStarSearch::AStarSearch(Graph<AssemblyData,EdgeData>& assembly)
   : assembly_(assembly)
-{};
+{}
 
-/* Perform the A* graph search.
-    @graph: pointer to graph on which the search should be performed.
-    @root: pointer to node at which the search should begin.
-    @exapnder: exapnder object used for node expansion.
-**/
-Node<SearchData>* AStarSearch::search(Graph<SearchData,EdgeData>& graph, Node<SearchData>* root, NodeExpander& expander)
+// Perform the graph search:
+//   @graph: pointer to graph on which the search should be performed.
+//   @root: pointer to node at which the search should begin.
+//   @exapnder: exapnder object used for node expansion.
+//
+Node<SearchData>* AStarSearch::search(Graph<SearchData,EdgeData>& graph, 
+                                Node<SearchData>* root, NodeExpander& expander)
 {
 
     Node<SearchData> *current = nullptr;
     std::priority_queue<Node<SearchData> *, std::vector<Node<SearchData> *>, LessThan> openSet;
 
-    // Closed-Set is redundant as the search is performed on a acyclic graph where every path is unique.
-    // -> Nodes can only be reached in one way.
-    // Not using the closed-set saves some processing time needed to lookuo-stuff.
-    // NodeSet closedSet; 
+    // Closed set is redundant as the search is performed on a acyclic graph where every path is unique.
+    // Nodes can only be reached in one way. Not using the closed-set saves some time used for lookups.
     expander.expandNode(root->id);
     root->data.h_score  = this->calc_hscore(root);
     root->data.f_score = this->calc_fscore(root);
